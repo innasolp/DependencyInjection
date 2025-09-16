076188fa0b6651bc6abda024664b64f4fc8b55c9
innasolp <innasolp@gmail.com>
2025-09-16 11:25:53 +0300

    DI-017 fixed

---
+++ DependencyInjection.ImplementationFactory/AssemblyExtensions.cs
@@ -0,0 +1,19 @@
+﻿using DependencyInjection.AssemblyExtensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DependencyInjection.ImplementationFactory;
+
+public static class AssemblyExtensions
+{
+    public static IServiceCollection AddServiceImplementationFactoriesFromPath(this IServiceCollection services, Type serviceType, string path)
+    {
+        var implementationFactories = typeof(IServiceImplementationFactory).GetImplementationsForInterface(path).
+            Where(t => t.IsGenericType &&
+                (t.GenericTypeArguments.Contains(serviceType) || t.GenericTypeArguments.Any(g => g.GetInterfaces().Contains(serviceType))));
+
+        foreach (var implementationFactory in implementationFactories)
+            services.AddServiceImplementationSingleton(serviceType, implementationFactory);
+
+        return services;
+    }
+}
