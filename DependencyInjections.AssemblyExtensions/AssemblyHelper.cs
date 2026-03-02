6e346aff01d4b045f6111a6296c37815a452be71
innasolp <innasolp@gmail.com>
2026-03-02 15:26:11 +0300

    #38 add method RegisterAllServiceImplementationFromPath

---
+++ DependencyInjections.AssemblyExtensions/AssemblyHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+namespace DependencyInjection.AssemblyExtensions;
+
+public static class AssemblyHelper
+{
+    public static IEnumerable<Assembly> GetAllAssembliesFromPath(string path)
+    {
+        var allFiles = Directory.EnumerateFiles(path, "*.dll", SearchOption.AllDirectories).ToArray();
+
+        var assemblies = new List<Assembly>();
+        foreach (var file in allFiles)
+        {
+            try
+            {
+                var assembly = Assembly.LoadFrom(file);
+                if (!assemblies.Contains(assembly))
+                    assemblies.Add(assembly);
+            }
+            catch (BadImageFormatException)
+            {
+                continue;
+            }
+            catch (FileLoadException)
+            {
+                continue;
+            }
+        }
+
+        return assemblies;
+    }
+}
---
+++ DependencyInjections.AssemblyExtensions/DIAssemblyExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DependencyInjection.AssemblyExtensions;
+
+public static class DIAssemblyExtensions
+{
+    public static IServiceCollection AddServiceImplementationsFromPath(this IServiceCollection services, Type serviceType, string path)
+    {
+        var implementationTypes = serviceType.GetImplementationsForInterface(path);
+        foreach (var implementationType in implementationTypes)
+            services.AddSingleton(serviceType, implementationType);
+
+        return services;
+    } 
+    
+    public static IServiceCollection RegisterAllServiceImplementationFromPath(this IServiceCollection services, string path)
+    {
+        var assemblies = AssemblyHelper.GetAllAssembliesFromPath(path);
+
+        foreach (var assembly in assemblies)
+        {
+            var registrations = assembly.GetTypes().Where(t => t.IsClass && !t.IsAbstract && t.GetInterfaces().Length > 0)
+            .Select(t => new
+            {
+                Implementation = t,
+                Services = t.GetInterfaces()
+            });
+
+            foreach (var reg in registrations)
+            {
+                foreach (var service in reg.Services)
+                {
+                    services.AddSingleton(service, reg.Implementation);
+                }
+            }
+        }
+
+        return services;
+    }
+}
